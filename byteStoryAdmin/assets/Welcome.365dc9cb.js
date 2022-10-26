62c506adfd9f126e6ca69d73f877aac123fff3cc
daemon <hoooost@163.com>
2022-10-26 15:44:15 +0800

    byteStoryAdmin

---
+++ byteStoryAdmin/assets/Welcome.365dc9cb.js
@@ -0,0 +1,9 @@
+import{_ as t,c as e,o,a as s}from"./index.7bd8a01f.js";const i={name:"welcome"},a={class:"welcome"},c=s(`<div class="content"><div class="sub-title">\u6B22\u8FCE\u4F53\u9A8C</div><div class="title">byteStory\u540E\u53F0\u7BA1\u7406\u7CFB\u7EDF</div><div class="desc"> - Vue3.0+ElementPlus+Node+Mongo\u6253\u9020\u901A\u7528\u540E\u53F0\u7BA1\u7406\u7CFB\u7EDF </div><!-- &lt;iframe
+        frameborder=&quot;no&quot;
+        border=&quot;0&quot;
+        marginwidth=&quot;0&quot;
+        marginheight=&quot;0&quot;
+        width=&quot;330&quot;
+        height=&quot;86&quot;
+        src=&quot;//music.163.com/outchain/player?type=2&amp;id=1878315329&amp;auto=0&amp;height=66&quot;
+      &gt;&lt;/iframe&gt; --></div><div class="img"></div>`,2),n=[c];function r(u,d,l,m,_,v){return o(),e("div",a,n)}var p=t(i,[["render",r],["__file","/Users/zhangfuchuan/Documents/byteStory/byteStoryAdmin/src/views/Welcome.vue"]]);export{p as default};
---
+++ byteStoryAdmin/assets/utils.e17d6760.js
@@ -0,0 +1,1 @@
+function c(t){const e=new Date(t),r=n(e.getFullYear()),o=n(e.getMonth()+1),a=n(e.getDate());return r+"-"+o+"-"+a}function n(t){return`${t}`.length===1?`0${t}`:t}export{c as g};
